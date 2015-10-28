f0580383cd977758a4794244d35fa7b34e0ec69a
BBarbosa <brunobarbosa.caos@gmail.com>
2015-10-28 01:21:43 +0000

    ready to parallelize

--- openmp/skel_seq.c
+++ openmp/skel_seq.c
@@ -2,64 +2,64 @@
 #include<stdlib.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<ctype.h>
 
 #define ROW 500        //define o máximo de linhas
 #define COL 500        //define o máximo de colunas
 
-int mat[ROW][COL];
+char tipo[3];          //tipo de ficheiro
+int mat[ROW][COL];     //matriz onde será guardada a imagem
 int linhas,colunas;    //garantir que linha < ROW e coluna < COL
                        //#linhas e #colunas efectivamente usadas
 
 /**
- * Carrega a imagem para a matriz
+ * Carrega imagem do tipo .pbm 
  * @param path
- * @return
+ * @return 
  */
-int carregaImagem(char *path) {
-    int fp,i,j,flag=1;
-    char b = '1';
-
-    fp = open(path,O_RDONLY);
+int carregaImagemPBM(char *path) {
+    FILE *fp;
+    //char tipo[3];
+    int l,c,i,j;
     
-    if(fp != -1) {
-        i = j = 0;
-        while(read(fp,&b,1)) {
-            if(b == '\n') {
-                if(flag) {
-                    colunas = j;
-                    flag = 0;
-                }
-                i++;
-                j=0;
-            } else {
-                mat[i][j] = atoi(&b);
-                j++;
+    fp = fopen(path,"r");
+    
+    if(fp) {
+        fscanf(fp,"%s",tipo);     //ok
+        fscanf(fp,"%d %d",&c,&l); //ok
+        linhas = l;
+        colunas = c;
+        printf("Tipo %s\nLinhas %d Colunas %d\n",tipo,l,c);
+        
+        for(i=0;i<l;i++) {
+            for(j=0;j<c;j++) {
+                fscanf(fp,"%d ",&mat[i][j]);
             }
         }
-        
-        linhas = i;
-        
-        printf("Resolucao: %d x %d\n",linhas,colunas)
-    } else {
-        printf("ERRO: Nao foi possivel ler a imagem\n");
     }
-    close(fp);
-   
+    fclose(fp);
+    
     return 1;
 }
 
 /**
- * Imprime a matriz (testes)
+ * Cria a imagem do esqueleto
  */
 void imprimeMatriz() {
     int i,j;
-
+    FILE *fp;
+    
+    fp = fopen("output.ascii.pbm","w");
+    fprintf(fp,"%s\n",tipo);
+    fprintf(fp,"%d %d\n",colunas,linhas);
+    
     for(i=0;i<linhas;i++) {
         for(j=0;j<colunas;j++) {
-            printf("%d",mat[i][j]);
+            fprintf(fp,"%d ",mat[i][j]);
         }
-        printf("\n");
+        fprintf(fp,"\n");
     }
+    fclose(fp);
 }
 
 /**
@@ -84,23 +84,21 @@ int trans(int act, int ant) {
 int main(int argc, char **argv) {
     int alterou=1,i,j,vizinhos,transicoes,complementos;
     
-    criaFicheiro("teste.txt",100);
-    
     /* le ficheiro de input - verificar nargs */
     if(argc != 2) {
         alterou = 0;
-        printf("#ERRO!");
+        printf("#ERRO: Insira uma imagem\n");
     } else {
-        carregaImagem("teste.txt");
+        carregaImagemPBM(argv[1]);
     }
 
     /* Processamento - iniciar timer */
     while(alterou) {
         alterou = 0;
-
         /* Primeira passagem */
         for(i=1; i<linhas-1; i++) {
             for(j=1; j<colunas-1; j++) {
+                /* Se o pixel for diferente de zero */
                 if(mat[i][j]) {
                     /* vizinhos */
                     vizinhos = mat[i][j+1] + mat[i+1][j+1] + mat[i+1][j] + mat[i+1][j-1] + mat[i][j-1] + mat[i-1][j-1] + mat[i-1][j] + mat[i-1][j+1];
@@ -125,6 +123,7 @@ int main(int argc, char **argv) {
         /* Segunda passagem */
         for(i=1; i<linhas-1; i++) {
             for(j=1; j<colunas-1; j++) {
+                /* Se o pixel for diferente de zero */
                 if(mat[i][j]) {
                     /* vizinhos */
                     vizinhos = mat[i][j+1] + mat[i+1][j+1] + mat[i+1][j] + mat[i+1][j-1] + mat[i][j-1] + mat[i-1][j-1] + mat[i-1][j] + mat[i-1][j+1];
